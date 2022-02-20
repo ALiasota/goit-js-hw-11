c237f8968c91f82d6a919601c79ecae207303ee9
Andrii Liasota <lyasota.andriy@gmail.com>
2022-02-20 21:31:07 +0200

    Start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import ImagesIpiServise from './js/fetchImages';
+
+
+
+
+const imagesIpiServise = new ImagesIpiServise;
+
+imagesIpiServise.fetchImages();
---
+++ src/js/fetchImages.js
@@ -0,0 +1,48 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '25783532-c25c49afce5183be9881181c4';
+const axios = require('axios').default;
+
+export default class ImagesIpiServise {
+    constructor() {
+        this.page = 1;
+        this.searchQuery = '';
+    }
+
+    async fetchImages() {
+        const searchParams = new URLSearchParams({
+            key: KEY,
+            q: this.searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: this.page,
+            per_page: 40,
+        });
+
+        const url = `${BASE_URL}?${searchParams}`;
+        console.log(url)
+
+        try {
+            const images = await axios.get(url);
+            return images;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    } 
+}
